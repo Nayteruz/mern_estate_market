aaf538d0e7395d61d5735efd9536cadae2888492
Nayteruz <nayteruz@gmail.com>
2023-12-20 23:38:29 +0700

    Добавил header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import SignUp from "./pages/SignUp.jsx";
 import About from "./pages/About.jsx";
 import Profile from "./pages/Profile.jsx";
 import NotFound from "./pages/NotFound.jsx";
+import Header from "./components/Header.jsx";
 
 const App = () => {
 
     return <BrowserRouter>
+        <Header/>
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/sign-in" element={<SignIn/>} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,36 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom"
+
+const Header = () => {
+
+	return (
+		<header className="bg-slate-200 shadow-md">
+			<div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+				<div className="font-bold text-sm sm:text-xl flex flex-wrap">
+					<span className="text-slate-500">Nuar</span>
+					<span className="text-slate-700">Estate</span>
+				</div>
+				<form className="bg-slate-100 p-3 rounded-lg flex items-center">
+					<input
+						type="text"
+						placeholder="Поиск"
+						className="bg-transparent focus:outline-none w-24 sm:w-64"/>
+					<FaSearch className="text-slate-600" />
+				</form>
+				<ul className="flex gap-4">
+					<li className="cursor-pointer hidden sm:inline text-slate-700 hover:underline">
+						<Link to="/">Home</Link>
+					</li>
+					<li className="cursor-pointer hidden sm:inline text-slate-700 hover:underline">
+						<Link to="/about">About</Link>
+					</li>
+					<li className="cursor-pointer hidden sm:inline text-slate-700 hover:underline">
+						<Link to="/sign-in">SignIn</Link>
+					</li>
+				</ul>
+			</div>
+		</header>
+	)
+}
+
+export default Header;
