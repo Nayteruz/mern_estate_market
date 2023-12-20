dd47134532658e043271e0b4171eb3243f05f12f
Nayteruz <nayteruz@gmail.com>
2023-12-21 00:57:15 +0700

    Добавил коннект до базы монго

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from "mongoose";
+
+dotenv.config();
+
+mongoose
+	.connect(process.env.API_URL)
+	.then(() => {
+		console.log('CONNECTED TO DB');
+	})
+	.catch((err) => {
+		console.error(`Connection error ${err}`)
+	})
+
 
 const PORT = 3000;
 
