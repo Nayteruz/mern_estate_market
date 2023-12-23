d42faa45c90325d246a5cb4affaee3b6c1b57f7d
Nayteruz <nayteruz@gmail.com>
2023-12-24 02:30:37 +0700

    добавил функцию для ошибок

--- api/index.js
+++ api/index.js
@@ -28,3 +28,14 @@ app.listen(PORT, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+	const statusCode = err.statusCode || 500;
+	const message = err.message || "Упс ошибка сервера"
+
+	return res.status(statusCode).json({
+		success: false,
+		statusCode,
+		message,
+	})
+})
