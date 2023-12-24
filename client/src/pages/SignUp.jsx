f3b174849c679e29a76b4d15618cefd823c9ba63
Nayteruz <nayteruz@gmail.com>
2023-12-24 17:34:49 +0700

    Закончил ui регистрации

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,28 @@
+import {Link} from "react-router-dom";
+
 const SignUp = () => {
 
 	return (
-		<div>
-			SignUp
+		<div className="p-3 max-w-lg mx-auto">
+			<h1 className="text-3xl text-center font-semibold my-7 ">Регистрация</h1>
+			<form className="flex flex-col gap-4">
+				<input type="text" placeholder="Имя пользователя"
+				className="border p-3 rounded-lg outline-none" id="username"
+				/>
+				<input type="email" placeholder="Email"
+				className="border p-3 rounded-lg outline-none" id="email"
+				/>
+				<input type="password" placeholder="Пароль"
+				className="border p-3 rounded-lg outline-none" id="password"
+				/>
+				<button type="submit" className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80">Зарегистрироваться</button>
+			</form>
+			<div className="flex gap-2 mt-5">
+				<p>Уже зарегистрованы?</p>
+				<Link to={'/sign-in'} >
+					<span className="text-blue-700">Авторизоваться</span>
+				</Link>
+			</div>
 		</div>
 	)
 }
